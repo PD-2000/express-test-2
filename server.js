d0a98069a270f3324a0a18d27a39f202f857cb9a
Pawel Domagala <domagalapawel11@gmail.com>
2023-06-15 19:45:51 +0200

    Remove unused code

--- server.js
+++ server.js
@@ -5,18 +5,10 @@ const app = express();
 
 app.engine('hbs', hbs({extname: 'hbs', layoutsDir: './views/layouts', defaultLayout: 'main'}));
 app.set('view engine', 'hbs');
-// app.use((req, res, next) => {
-//   res.show = (name) => {
-//     res.sendFile(path.join(__dirname, `/views/${name}`));
-//   };
-//   next();
-// });
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({extended: false}));
-// app.use(express.json());
 
 app.get('/', (req, res) => {
-  // res.show('index.html');
   res.render('index');
 });
 app.get('/about', (req, res) => {
@@ -31,16 +23,10 @@ app.get('/info', (req, res) => {
 app.get('/history', (req, res, next) => {
   res.render('history');
 });
-// app.get('/hello/:name', (req, res) => {
-//   res.send(`Hello ${req.params.name}.`);
-// });
 app.get('/hello/:name', (req, res) => {
   res.render('Hello', {name: req.params.name});
 });
 
-// app.post('/contact/send-message', (req, res) => {
-//   res.json(req.body);
-// });
 app.post('/contact/send-message', (req, res) => {
   const {author, sender, title, message} = req.body;
   if(author && sender && title && message)
@@ -56,24 +42,3 @@ app.use((req, res) => {
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });
-
-/*
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  if(req.url === '/' && req.method === 'GET'){
-    res.write('<h1>My first server!</h1>');
-    res.end();
-  }
-  else if(req.url === 'about/' && req.method === 'GET'){
-    res.write('<h1>About</h1>');
-    res.end();
-  }
-  // ...then similar other routes
-})
-
-server.listen(8000, (err) => {
-  if(err) return console.log('something bad happened', err);
-  console.log(`server is listening on ${8000}`);
-});
-*/
